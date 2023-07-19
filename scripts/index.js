a677003b59dc36a6d7d256a9bc92abffd99abe14
nitesh-dev <niteshdev547@gmail.com>
2023-07-19 10:30:41 +0530

    nav click added

--- scripts/index.js
+++ scripts/index.js
@@ -34,8 +34,26 @@ function landingSlide(value = -1){
 
 function landingAutoSlide(){
 
-    slide()
-    landingTimeoutId = setTimeout(landingSlide, 5000)
+    landingSlide()
+    landingTimeoutId = setTimeout(landingAutoSlide, 5000)
 }
 
+
+
+
+// ------------------------------- nav toggle --------------------------------
+
+const navButton = document.querySelector('#nav-button')
+const navPhone = document.querySelector('.nav-phone')
+let isNavVisible = false
+function toggleNav(){
+    isNavVisible = !isNavVisible
+
+    if(isNavVisible){
+        navPhone.classList.add('nav-show')
+    }else{
+        navPhone.classList.remove('nav-show')
+    }
+
+}
 
